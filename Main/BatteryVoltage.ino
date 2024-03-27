80e7277a69355984a591a3b367ba9ad87fcd6301
Thomas Farstrike <thomasverstreken@protonmail.com>
2024-03-27 20:10:06 +0100

    Keep average battery voltage
    Sometimes, there happens to be little variation in the voltage,
    which makes the heuristic think it's running on battery instead of without.
    
    This makes the device go to sleep to conserve power.
    
    By keeping an average of the last 10 voltage readings,
    those glitches are easily identified and dealt with.

--- Main/BatteryVoltage.ino
+++ Main/BatteryVoltage.ino
@@ -1,7 +1,23 @@
+// Keep a rolling average of the last 10 voltages
+double averageVoltage = 0;
+int averageVoltageN = 0;
 
 // These values are remembered to know what to erase upon the next draw
 int xPosOfHealthAndStatus = NOT_SPECIFIED;
 int yPosOfHealthAndStatus = NOT_SPECIFIED;
+
+double approxRollingAverage(double avg, double input) {
+  averageVoltageN++;
+  if (averageVoltageN>10) averageVoltageN=10;
+  avg -= avg/averageVoltageN;
+  avg += input/averageVoltageN;
+  //Serial.print("approxRollingAverage = " + String(averageVoltage,4));
+  return avg;
+}
+
+double getAverageBatteryVoltage() {
+  return averageVoltage;
+}
 
 // Returns the VBAT value.
 // If we're not on battery but USB powered, then it returns a negative VBAT value.
@@ -22,14 +38,15 @@ double getBatteryVoltage() {
 
     totalLevel = totalLevel / (nrReads+1); // one additional read is done for the prevValue
     totalDiff = totalDiff / nrReads;
-    Serial.println("Average battery level: " + String(totalLevel));
-    Serial.println("Average battery level diff: " + String(totalDiff));
+    //Serial.println("Average battery level: " + String(totalLevel));
+    //Serial.println("Average battery level diff: " + String(totalDiff));
 
     double voltage = (totalLevel * 1.72) / 1000;
     if (totalDiff > 32) {
       // The battery has a smoothing effect so lots of variance between levels means we're not on battery.
       voltage *= -1;
     }
+    averageVoltage = approxRollingAverage(averageVoltage, voltage);
     //return 3.7; // for testing low battery situation
     return voltage;
 }
