4908e47e0dfb328506936db13a24825ee12827b5
Thomas Farstrike <thomasverstreken@protonmail.com>
2024-03-29 11:45:01 +0100

    Increment version number

--- Main/Constants.h
+++ Main/Constants.h
@@ -37,7 +37,7 @@ const char * esWeekdays[] = { "D", "L", "M", "M", "J", "V", "S"};
 const char * enWeekdays[] = { "S", "M", "T", "W", "T", "F", "S"};
 const char * nlWeekdays[] = { "Z", "M", "D", "W", "D", "V", "Z"};
 
-String currentVersion = "2.2.2";
+String currentVersion = "2.2.3";
 
 typedef enum {
     STR2INT_SUCCESS,
