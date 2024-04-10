687e0db1badb462627153d47ea5eeb15082d6992
Thomas Farstrike <thomasverstreken@protonmail.com>
2024-04-10 11:04:10 +0200

    Use both hardware and display version for backwards compatibility

--- Main/Updates.ino
+++ Main/Updates.ino
@@ -38,6 +38,21 @@ String getShortHardwareInfo() {
   }
 }
 
+String getLongHardwareInfo() {
+  // Since the unified builds, we only know which display it is, not really which hardware.
+  // But to be backwards compatible with the update checker metrics, we assume
+  // a 2.13DEPG display must be the LILYGOT5V213
+  // and 2.66DEPG must be the LILYGOT5V266
+  int displayToUse = getDisplayToUse();
+  if (displayToUse == DISPLAY_TYPE_213DEPG) {
+    return "LILYGOT5V213|DEPG0213BN";
+  } else if (displayToUse == DISPLAY_TYPE_266DEPG) {
+    return "LILYGOT5V266|DEPG0266BN";
+  } else {
+    return "UNKNOWN|UNKNOWN";
+  }
+}
+
 String getShortVersion() {
   return currentVersion;
 }
@@ -45,7 +60,7 @@ String getShortVersion() {
 String getFullVersion() {
   const char compiletime[] = __DATE__ " " __TIME__;
   String compileTime(compiletime);
-  return currentVersion + "|" + getShortHardwareInfo() + "|" + compileTime;
+  return currentVersion + "|" + getLongHardwareInfo() + "|" + compileTime;
 }
 
 /*
